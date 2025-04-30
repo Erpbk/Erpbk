ab786bbafe2a5abcd8536f9cd41be1d137a6221a
Shoaib Rizwan <104502052+imshoaibrizwan@users.noreply.github.com>
2025-04-30 21:16:50 +0500

    email update

--- app/Http/Controllers/RidersController.php
+++ app/Http/Controllers/RidersController.php
@@ -396,7 +396,8 @@ class RidersController extends AppBaseController
 
       Mail::send('emails.general', $data, function ($message) use ($request) {
         $message->to([$request->email_to]);
-        $message->cc([env('ADMIN_CC_EMAIL')]);
+        $message->cc(env('ADMIN_CC_EMAIL'));
+        $message->bcc([env('ADMIN_CC_EMAIL')]);
         $message->replyTo([env('ADMIN_CC_EMAIL')]);
         $message->subject($request->email_subject);
         //$message->attachData($pdf->output(), $request->email_subject . '.pdf');
